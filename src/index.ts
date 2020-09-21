7ad7701d15cb1235ea61b20dfb581c54c19d53de
Justyn Boyer <justynboyer@gmail.com>
2020-09-22 00:07:14 +0100

    fix: switch to pkg

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-process-exit */
 /* eslint-disable @typescript-eslint/ban-ts-ignore */
 // import ascii from 'ascii-art';
 import boxen from 'boxen';
@@ -6,7 +7,6 @@ import 'colors';
 import {select} from 'input';
 import {exec} from 'child-process-promise';
 import cwd from 'cwd';
-import {compile} from 'nexe';
 import {windowClear} from './helpers';
 // ████████████████████████
 // ████████████████████████
@@ -59,31 +59,20 @@ windowClear();
           console.log(boxen('Compiling TS backdoor to JS...').gray);
           try {
             await exec('tsc');
+            await exec('mv build/darwin.js build/main.js');
             console.log(boxen('Done!').green);
           } catch (error) {
             console.log(boxen('ERROR!!!').red);
             console.error(error);
           }
           console.log(
-            boxen(
-              'Packaging JS backdoor to binary... (may take a while, please be patient!)'
-            ).gray + '\n'
+            "Now, run this in the project's root directory to compile your built JS file into an executable\n\nyarn pkg . --targets macos\n"
           );
-          try {
-            compile({
-              targets: [a],
-              input: cwd() + `/build/${a}.js`,
-              output: 'build/' + a,
-            });
-          } catch (error) {
-            console.log(boxen('ERROR!!!').red);
-            console.error(error);
-          }
+          process.exit(0);
         });
         break;
 
       default:
-        console.log('dafuq iz dis');
         break;
     }
   });
